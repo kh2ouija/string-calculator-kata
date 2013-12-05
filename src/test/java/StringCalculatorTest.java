ec233f268e640feb09baace629ddad1322f48a7c
kh2ouija <kh2ouija@yahoo.com>
2013-12-05 18:26:13 +0200

    initial import

---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,44 @@
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void shouldAcceptInputStrings() {}
+
+    @Test
+    public void shouldReturnZeroOnEmptyString() {}
+
+    @Test
+    public void shouldReturnSameOnSingleNumber() {}
+
+    @Test
+    public void shouldReturnSumOfTwoNumbersDelimitedByComma() {}
+
+    @Test
+    public void shouldReturnSumOfMultipleNumbersDelimitedByCommas() {}
+
+    @Test
+    public void shouldThrowExceptionOnNegativeInput() {}
+
+    @Test
+    public void shouldIgnoreNumbersLargerThanThousand() {}
+
+    @Test
+    public void shouldAllowNewlineDelimiter() {}
+
+    @Test
+    public void shouldAllowSpecifiedSingleCharacterDelimiter() {
+        // sample valid input: //:\n1:2 or //|\n1|2|3
+    }
+
+    @Test
+    public void shouldAllowSpecifiedDelimiterOfAnySize() {
+        // sample valid input: //[and]\n1and2
+    }
+
+    @Test
+    public void shouldAllowMultipleSpecifiedDelimitersOfAnySize() {
+        // sample valid input: //[and][also][+]\n1and2also3+4
+    }
+
+}
